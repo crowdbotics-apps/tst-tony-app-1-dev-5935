98dfdcb26ab738bf543355c319cc9d61c0f96c8d
Crowdbotics <team@crowdbotics.com>
2020-06-15 13:01:13 +0000

    Update components

--- src/features/BlankScreen45659/screens/blank.js
+++ src/features/BlankScreen45659/screens/blank.js
@@ -1,19 +1,55 @@
-import React from "react";
-import { View } from "react-native";
-import { withStyles, Text, Button, CheckBox, Radio, Toggle, Icon, Input } from 'react-native-ui-kitten';
+import React from "react"
+import { View } from "react-native"
+import {
+  withStyles,
+  Text,
+  Button,
+  CheckBox,
+  Radio,
+  Toggle,
+  Icon,
+  Input
+} from "react-native-ui-kitten"
 
 export class _Blank extends React.Component {
   static navigationOptions = {
     title: "Blank".toUpperCase()
-  };
+  }
 
-  state = { }
+  state = {}
 
-  render = () => <View style={this.props.themedStyle.container}></View>;
+  render = () => (
+    <View
+      editableName="View1"
+      style={{
+        overflow: "visible",
+        textAlign: "left",
+        verticalAlign: "baseline",
+        flexDirection: "column",
+        flexWrap: "wrap",
+        justifyContent: "flex-start",
+        alignItems: "stretch",
+        alignContent: "stretch",
+        borderStyle: "solid",
+        backgroundSize: "auto"
+      }}
+    >
+      <Image
+        resizeMethod="cover"
+        editableName="Image2"
+        style={{
+          overflow: "visible",
+          textAlign: "left",
+          verticalAlign: "baseline",
+          borderStyle: "solid"
+        }}
+      />
+    </View>
+  )
 }
 
 export default Blank = withStyles(_Blank, theme => ({
   container: {
     backgroundColor: theme["color-basic-100"]
   }
-}));
+}))
